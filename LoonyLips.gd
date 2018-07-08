1094f11efc8aea29d2661e877a73f3525f5ef842
shorinbonsai <shorinbonsai@gmail.com>
2018-07-07 22:23:30 -0400

    Initial commit

---
+++ LoonyLips.gd
@@ -0,0 +1,6 @@
+extends Node2D
+
+func _ready():
+	var prompt = ["James", "car", "hat", "sad"]
+	var story = "Once upon a time %s took his %s and accidently smooshed a %s.  This made him feel very %s."
+	
