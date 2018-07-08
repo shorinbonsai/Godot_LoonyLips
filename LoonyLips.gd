f198acf80826c2e9d57d9883d8176299e73bc46d
shorinbonsai <shorinbonsai@gmail.com>
2018-07-07 22:28:45 -0400

    testing arrays

--- LoonyLips.gd
+++ LoonyLips.gd
@@ -3,4 +3,4 @@ extends Node2D
 func _ready():
 	var prompt = ["James", "car", "hat", "sad"]
 	var story = "Once upon a time %s took his %s and accidently smooshed a %s.  This made him feel very %s."
-	
+	print (story % prompt)
