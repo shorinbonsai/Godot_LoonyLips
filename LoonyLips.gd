e50cb81031e948e9e80a38bc1f7fb8c758718608
shorinbonsai <shorinbonsai@gmail.com>
2018-07-09 14:51:13 -0400

    added blackboard texture and text box

--- LoonyLips.gd
+++ LoonyLips.gd
@@ -4,3 +4,15 @@ func _ready():
 	var prompt = ["James", "car", "hat", "sad"]
 	var story = "Once upon a time %s took his %s and accidently smooshed a %s.  This made him feel very %s."
 	print (story % prompt)
+	$Blackboard/StoryText.text = story % prompt
+	#get_node("Blackboard/StoryText")
+		
+func _on_TextureButton_pressed():
+	var new_text = $Blackboard/TextBox.get_text()
+	_on_TextBox_text_entered(new_text)
+
+func _on_TextBox_text_entered(new_text):
+	$Blackboard/StoryText.text = new_text
+	$Blackboard/TextBox.clear()
+
+
