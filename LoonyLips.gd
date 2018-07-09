a3fcdf20bb64896cc82901c4ff43b5e323ccb127
shorinbonsai <shorinbonsai@gmail.com>
2018-07-09 15:21:44 -0400

    add player input to array

--- LoonyLips.gd
+++ LoonyLips.gd
@@ -1,18 +1,21 @@
 extends Node2D
 
+var player_words = [] #player inputs
+var prompt = ["a name", "a thing", "a feeling", "another feeling", "some things"]
+var story = "Once upon a time %s took his %s and accidently smooshed it. They felt %s.  This made him feel very %s but there was still a %s."
+
 func _ready():
-	var prompt = ["James", "car", "hat", "sad"]
-	var story = "Once upon a time %s took his %s and accidently smooshed a %s.  This made him feel very %s."
 	print (story % prompt)
 	$Blackboard/StoryText.text = story % prompt
 	#get_node("Blackboard/StoryText")
+	$Blackboard/TextBox.text = ""
 		
 func _on_TextureButton_pressed():
 	var new_text = $Blackboard/TextBox.get_text()
 	_on_TextBox_text_entered(new_text)
 
 func _on_TextBox_text_entered(new_text):
-	$Blackboard/StoryText.text = new_text
+	player_words.append(new_text)
 	$Blackboard/TextBox.clear()
-
+	print(player_words)
 
