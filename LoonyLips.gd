996ac2d7b5519d5d60dcaf9cb5e7e5577fe3092a
shorinbonsai <shorinbonsai@gmail.com>
2018-07-24 22:44:21 -0400

    moxed text to json files

--- LoonyLips.gd
+++ LoonyLips.gd
@@ -1,15 +1,28 @@
 extends Node2D
 
 var player_words = [] #player inputs
-var current_story = {
-		"prompt" : ["a name", "a thing", "a feeling", "another feeling", "some things"],
-		"story" : "Once upon a time %s took his %s and accidently smooshed it. They felt %s.  This made him feel very %s but there was still a %s."
-		}
+var current_story
+var strings #all the text thats displayed to player
 
 func _ready():
-
+	set_random_story()
+	strings = get_from_json("other_strings.json")
+	$Blackboard/StoryText.text = strings["intro_text"]
+	prompt_player()
 	$Blackboard/TextBox.text = ""
-	$Blackboard/StoryText.text = ("Welcome to Loony Lips! \n\nLet's have fun creating stories together.\n\nCan I have " + current_story.prompt[player_words.size()] + ", please?")
+
+func set_random_story():
+	var stories = get_from_json("stories.json")
+	randomize()
+	current_story = stories [randi() % stories.size()]
+	
+func get_from_json(filename):
+	var file = File.new() #the file object
+	file.open(filename, File.READ) #Assume the file exists
+	var text = file.get_as_text()
+	var data = parse_json(text)
+	file.close()
+	return data
 
 		
 func _on_TextureButton_pressed():
@@ -21,14 +34,16 @@ func _on_TextureButton_pressed():
 
 func _on_TextBox_text_entered(new_text):
 	player_words.append(new_text)
-	$Blackboard/TextBox.clear()
+	$Blackboard/TextBox.text = ""
+	$Blackboard/StoryText.text = ""
 	check_player_word_length()
 	
 func is_story_done():
 	return player_words.size() == current_story.prompt.size()
 	
 func prompt_player():
-	$Blackboard/StoryText.text = ("Can I have " + current_story.prompt[player_words.size()] + ", please?")
+	var next_prompt = current_story["prompt"][player_words.size()]
+	$Blackboard/StoryText.text += (strings["prompt"] % next_prompt)
 
 func check_player_word_length():
 	if is_story_done():
@@ -38,7 +53,7 @@ func check_player_word_length():
 
 func tell_story():
 	$Blackboard/StoryText.text = current_story.story % player_words
-	$Blackboard/TextureButton/ButtonLabel.text = "Again?"
+	$Blackboard/TextureButton/ButtonLabel.text = strings["again"]
 	end_game()
 	
 func end_game():
